27b0da40f3241051a16afbdd3cd1bd9956ec696f
Ethan <kkojaeh@gmail.com>
2018-09-11 11:11:10 +0900

    메뉴 삭제

--- quotation-api/src/main/java/pico/erp/quotation/ROLE.java
+++ quotation-api/src/main/java/pico/erp/quotation/ROLE.java
@@ -1,29 +1,18 @@
 package pico.erp.quotation;
 
-import java.util.Collections;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.persistence.Id;
-import javax.persistence.Transient;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import pico.erp.shared.data.Menu;
 import pico.erp.shared.data.Role;
 
 @RequiredArgsConstructor
 public enum ROLE implements Role {
 
-  QUOTATION_MANAGER(Stream.of(MENU.QUOTATION_MANAGEMENT).collect(Collectors.toSet())),
-  QUOTATION_ACCESSOR(Collections.emptySet());
+  QUOTATION_MANAGER,
+  QUOTATION_ACCESSOR;
 
   @Id
   @Getter
   private final String id = name();
 
-  @Transient
-  @Getter
-  @NonNull
-  private Set<Menu> menus;
 }
